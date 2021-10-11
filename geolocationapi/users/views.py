0b6aa9669db9f3329cf3dc14e0824e215fb1047d
Damian Zbrożek <damianzbrozek@hotmail.com>
2021-10-11 17:53:53 +0200

    fix: added missing description

--- geolocationapi/users/views.py
+++ geolocationapi/users/views.py
@@ -1,9 +1,13 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 
 from .serializers import SignupSerializer
 
 
+@extend_schema_view(
+    post=extend_schema(description='Creates a new user account'),
+)
 class SignUpUserView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = SignupSerializer
